9323c378674d38df0deb38a6122d5acbc76d2229
gxuecai <842633836@qq.com>
2020-06-01 18:31:39 +0800

    initial user search ca combo file

---
+++ search_ca_combo.py
@@ -0,0 +1,35 @@
+import xml_parser_rfc
+import re
+import os
+
+# parse the lte band number as B1+B3+B4 format
+def parse_ca_bands_from_input(input_string):
+    band_list_return=[]
+    split_input= re.split(r'\+', input_string)
+    for band_string in split_input:
+        band = re.search(r'[Bb]([0-9]+)', band_string)
+        band_list_return.append(band.group(1))
+        band_list_return.sort()
+    return band_list_return
+
+# -----------------------------Test-------------------------------
+#print(parse_ca_bands_from_input("B1+B3+B5"))
+
+# -----------------------------Main-------------------------------
+os.system("cls") # cmd window clear screen
+
+user_search_combo=""
+while 1:
+    user_search_combo = input("Please input search combos as the format B1+B2+B4 >>")
+    if(user_search_combo == 'exit'):
+        break
+    input_band_combo = parse_ca_bands_from_input(user_search_combo)
+    print('-------------------------------------------------- \nYour input band list: ', input_band_combo)
+
+    # find matched ca combos from RFC parser ca list
+    for ca_combos_i in xml_parser_rfc.lte_combo_list:
+        if input_band_combo == ca_combos_i.band_list:
+            print(ca_combos_i.ca_string)
+    print('-------------------------------------------------- \n')
+
+os.system("pause") # cmd window pause screen
